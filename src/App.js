6241b7afbe90e026f3ac5489bda2e8b7c7886395
ol3gatron <olegatron@gmail.com>
2022-05-10 16:07:16 +0300

    created Header, FeedbackItem and FeedbackList components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
+import {useState} from "react"
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from "./data/FeedbackData"
+
 function App() {
-  const title = "My blog"
-  const body = "This is my blogpost"
-  const comments = [
-    { id: 1, text: "First Comment" },
-    { id: 2, text: "Second Comment" },
-    { id: 3, text: "Third Comment" },
-  ]
-
-  const loading = false
-  const showComments = true
-
-  if (loading) return <h1>Loading...</h1>
-
-  const commentsBlock = (
-    <div className="comments">
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  )
-
+  const [feedback, setFeedback] = useState(FeedbackData)
 
   return (
     <>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-      {showComments && commentsBlock}
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback}/>
+      </div>
     </>
-
   )
 }
 
---
+++ src/data/FeedbackData.jsx
@@ -0,0 +1,39 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum optio officia culpa consequatur. Praesentium in eligendi rerum nulla, explicabo at nihil consectetur vel possimus inventore excepturi, nostrum nesciunt maiores amet.",
+  },
+  {
+    id: 2,
+    rating: 7,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum optio officia culpa consequatur. Praesentium in eligendi rerum nulla, explicabo at nihil consectetur vel possimus inventore excepturi, nostrum nesciunt maiores amet.",
+  },
+  {
+    id: 3,
+    rating: 7,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum optio officia culpa consequatur. Praesentium in eligendi rerum nulla, explicabo at nihil consectetur vel possimus inventore excepturi, nostrum nesciunt maiores amet.",
+  },
+  {
+    id: 4,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum optio officia culpa consequatur. Praesentium in eligendi rerum nulla, explicabo at nihil consectetur vel possimus inventore excepturi, nostrum nesciunt maiores amet.",
+  },
+  {
+    id: 5,
+    rating: 6,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum optio officia culpa consequatur. Praesentium in eligendi rerum nulla, explicabo at nihil consectetur vel possimus inventore excepturi, nostrum nesciunt maiores amet.",
+  },
+  {
+    id: 6,
+    rating: 1,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum optio officia culpa consequatur. Praesentium in eligendi rerum nulla, explicabo at nihil consectetur vel possimus inventore excepturi, nostrum nesciunt maiores amet.",
+  },
+  {
+    id: 7,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum optio officia culpa consequatur. Praesentium in eligendi rerum nulla, explicabo at nihil consectetur vel possimus inventore excepturi, nostrum nesciunt maiores amet.",
+  },
+]
+
+export default FeedbackData
