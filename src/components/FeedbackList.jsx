985e7a1b42a4cb6ec8077ef33544120c38e1c792
ol3gatron <olegatron@gmail.com>
2022-05-14 18:53:14 +0300

    added feedback stats component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,8 @@
+import {PropTypes} from "prop-types"
+
 import FeedbackItem from "./FeedbackItem"
 
-function FeedbackList({feedback}) {
+function FeedbackList({feedback, handleDelete}) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
@@ -8,9 +10,19 @@ function FeedbackList({feedback}) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
+  )
+}
+
+FeedbackItem.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
   )
 }
 
