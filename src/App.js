d67cec0bb71c93be26c8f8312eebd53ecf6a5586
ol3gatron <olegatron@gmail.com>
2022-07-25 16:46:50 +0300

    moved addFeedback and deleteFeedback functions to FeedbackContext

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import {v4 as uuidv4} from "uuid"
-import {useState} from "react"
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
-import FeedbackData from "./data/FeedbackData"
 import FeedbackForm from "./components/FeedbackForm"
 import AboutPage from "./pages/AboutPage"
 import AboutIconLink from "./components/AboutIconLink"
 import { FeedbackProvider } from "./context/FeedbackContext"
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-  }
-
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
-
   return (
     <FeedbackProvider>>
       <Router>
@@ -32,9 +16,9 @@ function App() {
           <Routes>
             <Route exact path="/" element={
               <>
-                <FeedbackForm  handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList  handleDelete={deleteFeedback}/>
+                <FeedbackForm />
+                <FeedbackStats />
+                <FeedbackList />
               </>
             }>
             </Route>
