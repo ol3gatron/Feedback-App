6d4c0b2d78994619cfbec20f003ddbeac1c2aa6b
ol3gatron <olegatron@gmail.com>
2022-07-26 12:13:08 +0300

    added fetchFeedback function that fetches data from json-server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,30 +1,29 @@
-import {v4 as uuidv4} from "uuid"
-import { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid"
+import { createContext, useState, useEffect } from "react"
 
 const FeedbackContext = createContext()
 
-export const FeedbackProvider = ({children}) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This is feedback item 1",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "This is feedback item 2",
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: "This is feedback item 3",
-      rating: 8,
-    },
-  ])
+export const FeedbackProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      "http://localhost:5000/feedback?_sord=id&_order=desc"
+    )
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
@@ -38,7 +37,9 @@ export const FeedbackProvider = ({children}) => {
   }
 
   const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) => (item.id === id ? {...item, ...updItem} : item)))
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    )
   }
 
   const editFeedback = (item) => {
@@ -48,16 +49,21 @@ export const FeedbackProvider = ({children}) => {
     })
   }
 
-  return <FeedbackContext.Provider value={{
-    feedback,
-    feedbackEdit,
-    deleteFeedback,
-    addFeedback,
-    editFeedback,
-    updateFeedback,
-  }}>
-    {children}
-  </FeedbackContext.Provider>
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        feedbackEdit,
+        isLoading,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        updateFeedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  )
 }
 
 export default FeedbackContext
